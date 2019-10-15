a18ae187d495da62c9bbdcbd6fe05d2248afd11b
Christopher Mark Gore <cgore@cgore.com>
2019-10-15 12:49:50 -0500

    Copying over the implementation from `evolve`

--- src/rosenbrock/core.clj
+++ src/rosenbrock/core.clj
@@ -1,6 +1,24 @@
-(ns rosenbrock.core)
+(ns rosenbrock.core
+  "In mathematical optimization, the Rosenbrock function is a non-convex function,
+  introduced by Howard H. Rosenbrock in 1960, which is used as a performance
+  test problem for optimization algorithms.
 
-(defn foo
-  "I don't do a whole lot."
+  https://en.wikipedia.org/wiki/Rosenbrock_function"
+  (:require [clojure.math.numeric-tower :refer [expt]]))
+
+(defn **2
+  "Uses the numeric tower expt to square a number"
   [x]
-  (println x "Hello, World!"))
+  (expt x 2))
+
+(defn original
+  "Original 2D variant of the Rosenbrock function.  The parameters `a` and `b` are
+  to be fixed, and exploration occurs on `x` and `y`.  Often these parameters
+  are set such that `a` is `1` and `b` is `100`, so if no `a` or `b` is
+  specified then they are defaulted to that.  Otherwise, consider using
+  `partial` to construct a version with the `a` and `b` you wish."
+  ([x y]
+   (original 1.0 100.0 x y))
+  ([a b x y]
+   (+ (**2 (- a x))
+      (* b (**2 (- y (**2 x)))))))
--- test/rosenbrock/core_test.clj
+++ test/rosenbrock/core_test.clj
@@ -1,7 +1,11 @@
 (ns rosenbrock.core-test
   (:require [clojure.test :refer :all]
-            [rosenbrock.core :refer :all]))
+            [rosenbrock.core :as rosenbrock]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest original
+  (testing "global minimum with default `a` and `b` should be at f(x=1,y=1)"
+    (is (zero? (rosenbrock/original 1 1)))
+    (is (not (zero? (rosenbrock/original 5 7)))))
+  (testing "setting `a` to 0 will make the global minimum at the origin"
+    (is (zero? (rosenbrock/original 0 100 0 0)))
+    (is (not (zero? (rosenbrock/original 0 100 1 1))))))
