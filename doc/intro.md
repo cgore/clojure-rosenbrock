e40da1886e79523d811aa6223fb597c05dd539cd
Christopher Mark Gore <chris.gore@rate.com>
2019-10-15 12:42:46 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rosenbrock
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rosenbrock/core_test.clj
@@ -0,0 +1,7 @@
+(ns rosenbrock.core-test
+  (:require [clojure.test :refer :all]
+            [rosenbrock.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
